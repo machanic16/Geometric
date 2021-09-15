db1af46e04ed728496a776111b7eba468f3d1e5f
Joel Tovar <jatr160397@gmail.com>
2021-09-15 12:45:28 -0400

    Implement the logic for get the angles in a triangle and the area using the length of the three sides

--- js/index.js
+++ js/index.js
@@ -2,13 +2,16 @@
 
 // geting values from the user
 const inputA = document.getElementById("side-a");
-const sideA = parseFloat(inputA.value);
+let sideA = 0;
 
 const inputB = document.getElementById("side-b");
-const sideB = parseFloat(inputB.value);
+let sideB = 0;
 
 const inputC = document.getElementById("side-c");
-const sideC = parseFloat(inputC.value);
+let sideC = 0;
+
+//declaring variables to set the value later
+let perimeter = 0;
 
 // ***first of all, validate that the numbers correspon with the sides of a triangle***
 function isTriangle(sideA, sideB, sideC) {
@@ -20,15 +23,60 @@ function isTriangle(sideA, sideB, sideC) {
   return firstInequality && secondInequality && thirdInequality;
 }
 
-if (isTriangle(sideA, sideB, sideC)) {
-} else {
-  console.log("The numbers that you type do not make a triangle. ");
-}
-
-// calculate the perimeter
-
 function perimeterTriangel(sideA, sideB, sideC) {
   return sideA + sideB + sideC;
 }
-const perimeter = perimeterTriangel(sideA, sideB, sideC);
-console.log("The perimeter of your triangle is: " + perimeter);
+
+function calculate() {
+  if (isTriangle(sideA, sideB, sideC)) {
+    // calculate the perimeter
+    perimeter = perimeterTriangel(sideA, sideB, sideC);
+    console.log("The perimeter of your triangle is: " + perimeter);
+  } else {
+    console.log("The numbers that you type do not make a triangle. ");
+  }
+}
+
+function getSides() {
+  sideA = parseFloat(inputA.value);
+  sideB = parseFloat(inputB.value);
+  sideC = parseFloat(inputC.value);
+  calculate();
+}
+
+function angleBetween(sideVertice1, sideVertice2, sideOpositeToAngle) {
+  // This function uses the he cosin law to find the angle between tú side of the triangle.
+  // Parameters: side1 and side2 are the side other vértice to find the angle.
+  // Side3 is the opposite side to the angle
+  // C^ = A^2 + B^2 - 2ABcos(∆)
+  // After math
+  //Cos(∆) = (A^2 + B^2 -C^2)/(2AB)
+
+  let deltaInradians = Math.acos(
+    (sideVertice1 ** 2 + sideVertice2 ** 2 - sideOpositeToAngle ** 2) /
+      (2 * sideVertice1 * sideVertice2)
+  );
+
+  //let deltaInDecigrades = (deltaInradians * 360) / (2 * Math.PI);
+  /*console.log(
+    Math.acos((vertice1 ** 2 + vertice2 ** 2 - sideOpositeToAngle ** 2) / 2)
+  );*/
+
+  return deltaInradians;
+}
+
+function area(side1, side2, side3) {
+  /* in order to calculate the area of any triangle with the 3 sides
+  we need to know the angle between two side (above funtion)
+  Then the area would be: H * B / 2
+  where B: any side 
+        H: other side of the triangle multiplied by sen(delta)
+        delta: the angle between the two choosen sides
+  */
+  const delta = angleBetween(side1, side2, side3);
+  const base = side1;
+  const heigth = side2 * Math.sin(delta);
+  const area = (base * heigth) / 2;
+
+  return area;
+}
