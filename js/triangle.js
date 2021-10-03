7b05b8a4dd25efda8c17b5cba013f92fe2d2f4c6
Joel Tovar <jatr160397@gmail.com>
2021-10-03 13:38:29 -0400

    the triangle calculator works for the case that you have 3 sides

--- js/triangle.js
+++ js/triangle.js
@@ -1,6 +1,6 @@
-//Here is the logic for the calculator
+//Here is the logic for the Triangle calculator
 
-// geting values from the user
+// Geting values from the user
 const inputA = document.getElementById("side-a");
 let sideA = 0;
 
@@ -10,8 +10,24 @@ let sideB = 0;
 const inputC = document.getElementById("side-c");
 let sideC = 0;
 
-//declaring variables to set the value later
+// Declaring variables to set the value later
 let perimeter = 0;
+let area = 0;
+let oppositeToA = 0;
+let oppositeToB = 0;
+let oppositeToC = 0;
+
+// Getting fields where the answer will be shown
+// Perimeter
+const perimeterField = document.getElementById("perimeter");
+
+// Area
+const areaField = document.getElementById("area");
+
+// Angles fields
+const oppositeAField = document.getElementById("opposite-a");
+const oppositeBField = document.getElementById("opposite-b");
+const oppositeCField = document.getElementById("opposite-c");
 
 // ***first of all, validate that the numbers correspon with the sides of a triangle***
 function isTriangle(sideA, sideB, sideC) {
@@ -27,16 +43,6 @@ function perimeterTriangel(sideA, sideB, sideC) {
   return sideA + sideB + sideC;
 }
 
-function calculate() {
-  if (isTriangle(sideA, sideB, sideC)) {
-    // calculate the perimeter
-    perimeter = perimeterTriangel(sideA, sideB, sideC);
-    console.log("The perimeter of your triangle is: " + perimeter);
-  } else {
-    console.log("The numbers that you type do not make a triangle. ");
-  }
-}
-
 function getSides() {
   sideA = parseFloat(inputA.value);
   sideB = parseFloat(inputB.value);
@@ -45,7 +51,7 @@ function getSides() {
 }
 
 function angleBetween(sideVertice1, sideVertice2, sideOpositeToAngle) {
-  // This function uses the he cosin law to find the angle between tú side of the triangle.
+  // This function uses the he cosin law to find the angle between to side of the triangle.
   // Parameters: side1 and side2 are the side other vértice to find the angle.
   // Side3 is the opposite side to the angle
   // C^ = A^2 + B^2 - 2ABcos(∆)
@@ -65,7 +71,7 @@ function angleBetween(sideVertice1, sideVertice2, sideOpositeToAngle) {
   return deltaInradians;
 }
 
-function area(side1, side2, side3) {
+function areaValue(side1, side2, side3) {
   /* in order to calculate the area of any triangle with the 3 sides
   we need to know the angle between two side (above funtion)
   Then the area would be: H * B / 2
@@ -79,4 +85,34 @@ function area(side1, side2, side3) {
   const area = (base * heigth) / 2;
 
   return area;
+}
+
+function calculate() {
+  if (isTriangle(sideA, sideB, sideC)) {
+    // calculate the perimeter
+    perimeter = perimeterTriangel(sideA, sideB, sideC);
+    perimeterField.innerText = perimeter;
+
+    //calculate de area
+    area = areaValue(sideA, sideB, sideC);
+    areaField.innerText = area;
+
+    // calculate angles for the triangle
+    oppositeToA = angleBetween(sideB, sideC, sideA);
+    oppositeToA = (oppositeToA * 180) / Math.PI;
+    oppositeToA = oppositeToA.toFixed(2);
+    oppositeAField.innerText = oppositeToA;
+
+    oppositeToB = angleBetween(sideA, sideC, sideB);
+    oppositeToB = (oppositeToB * 180) / Math.PI;
+    oppositeToB = oppositeToB.toFixed(2);
+    oppositeBField.innerText = oppositeToB;
+
+    oppositeToC = angleBetween(sideA, sideB, sideC);
+    oppositeToC = (oppositeToC * 180) / Math.PI;
+    oppositeToC = oppositeToC.toFixed(2);
+    oppositeCField.innerText = oppositeToC;
+  } else {
+    console.log("The numbers that you type do not make a triangle. ");
+  }
 }
